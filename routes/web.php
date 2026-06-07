bacc1f8a5d5ee821b6cb1d84ab2c7434c8db6216
vinicius-araujo777 <vinicius.araujo60@aluno.ce.gov.br>
2026-06-07 15:26:08 -0300

    simplificando rotas

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\FornecedorController;
 use App\Http\Controllers\CafeController;
+
 require __DIR__.'/auth.php';
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -21,25 +25,3 @@ Route::middleware('auth')->group(function () {
     Route::resource('fornecedores', FornecedorController::class);
     Route::resource('cafes', CafeController::class);
 });
-
-Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
-Route::get('/categorias/create', [CategoriaController::class, 'create'])->name('categorias.create');
-Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
-Route::get('/categorias/{id}/edit', [CategoriaController::class, 'edit'])->name('categorias.edit');
-Route::put('/categorias/{id}', [CategoriaController::class, 'update'])->name('categorias.update');
-Route::delete('/categorias/{id}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
-
-Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('fornecedores.index');
-Route::get('/fornecedores/create', [FornecedorController::class, 'create'])->name('fornecedores.create');
-Route::post('/fornecedores', [FornecedorController::class, 'store'])->name('fornecedores.store');
-Route::get('/fornecedores/{id}/edit', [FornecedorController::class, 'edit'])->name('fornecedores.edit');
-Route::put('/fornecedores/{id}', [FornecedorController::class, 'update'])->name('fornecedores.update');
-Route::delete('/fornecedores/{id}', [FornecedorController::class, 'destroy'])->name('fornecedores.destroy');
-
-
-Route::get('/cafes', [CafeController::class, 'index'])->name('cafes.index');
-Route::get('/cafes/create', [CafeController::class, 'create'])->name('cafes.create');
-Route::post('/cafes', [CafeController::class, 'store'])->name('cafes.store');
-Route::get('/cafes/{id}/edit', [CafeController::class, 'edit'])->name('cafes.edit');
-Route::put('/cafes/{id}', [CafeController::class, 'update'])->name('cafes.update');
-Route::delete('/cafes/{id}', [CafeController::class, 'destroy'])->name('cafes.destroy');
