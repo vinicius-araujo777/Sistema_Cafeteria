a9fc122fcc943d23211dbf092cd488ac5a23f8af
vinicius-araujo777 <vinicius.araujo60@aluno.ce.gov.br>
2026-06-20 19:46:02 -0300

    adicionando o vite no docker e modificando o vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 });
